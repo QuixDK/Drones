b90e29a8d530adfcf2bad56796a7b00d97f76ee3
dstroganov <dstroganov@dynamika.ru>
2025-06-01 23:23:42 +0700

    Запретил задавать полетное задание вне радиуса док-станции и сделал подбор оптимального для задания дрона (тест)

--- drones/src/main/java/ru/ssugt/drones/service/FlightMissionService.java
+++ drones/src/main/java/ru/ssugt/drones/service/FlightMissionService.java
@@ -5,6 +5,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import ru.ssugt.drones.api.dto.FlightMission;
 import ru.ssugt.drones.jpa.entities.Drone;
+import ru.ssugt.drones.jpa.entities.Location;
 import ru.ssugt.drones.jpa.repository.DroneRepository;
 import ru.ssugt.drones.rabbit.CentralServer;
 
@@ -25,22 +26,59 @@ public class FlightMissionService {
 
     @Async
     public void requestDockStationStatus(FlightMission flightMission) throws IOException {
-        Drone drone = droneRepository.findById("bd760580-a87a-40b1-a362-4e77c8be3131").get();
-        for (List<Double> point: flightMission.getPoints()) {
-            drone.getLocation().setLatitude(point.get(0));
-            drone.getLocation().setLongitude(point.get(1));
-            drone = droneRepository.save(drone);
+        if (flightMission.getPoints().isEmpty()) {
+            return;
+        }
+
+        // Берем первую точку маршрута для поиска ближайшего дрона
+        List<Double> firstPoint = flightMission.getPoints().get(0);
+        double targetLat = firstPoint.get(0);
+        double targetLon = firstPoint.get(1);
+
+        // Находим ближайшего дрона к первой точке маршрута
+        Drone nearestDrone = findNearestDrone(targetLat, targetLon);
+
+        if (nearestDrone == null) {
+            throw new RuntimeException("No available drones found");
+        }
+
+        // Имитируем движение дрона по точкам маршрута
+        for (List<Double> point : flightMission.getPoints()) {
+            nearestDrone.getLocation().setLatitude(point.get(0));
+            nearestDrone.getLocation().setLongitude(point.get(1));
+            nearestDrone = droneRepository.save(nearestDrone);
+
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
-//        List<DockStationCallback> dockStationCallbacks = centralServer.requestStatus();
-//        //Поиск ближайшей станции со свободными дронами
-//        DockStationCallback dockStationCallback = dockStationCallbacks.get(0);
-//        centralServer.sendTask(dockStationCallback.getStationId(), flightMission);
+    }
 
+    private Drone findNearestDrone(double targetLat, double targetLon) {
+        List<Drone> allDrones = droneRepository.findAll();
+
+        Drone nearestDrone = null;
+        double minDistance = Double.MAX_VALUE;
+
+        for (Drone drone : allDrones) {
+            Location location = drone.getLocation();
+            if (location == null) continue;
+
+            double droneLat = location.getLatitude();
+            double droneLon = location.getLongitude();
+
+            double distance = Math.sqrt(Math.pow(droneLat - targetLat, 2) +
+                    Math.pow(droneLon - targetLon, 2));
+
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearestDrone = drone;
+            }
+        }
+
+        return nearestDrone;
     }
 
     public FlightMission generateFlightMission(List<List<Double>> points) {
