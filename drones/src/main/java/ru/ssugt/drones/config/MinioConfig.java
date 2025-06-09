aa914013206c04463b88d0f1e606575eb49a2334
dstroganov <dstroganov@dynamika.ru>
2025-06-09 23:46:23 +0700

    Определил minioEnpoint через enviroment

--- drones/src/main/java/ru/ssugt/drones/config/MinioConfig.java
+++ drones/src/main/java/ru/ssugt/drones/config/MinioConfig.java
@@ -1,16 +1,19 @@
 package ru.ssugt.drones.config;
 
 import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinioConfig {
 
+    @Value("${spring.minio.endpoint}")
+    String minioUrl = "";
     @Bean
     public MinioClient minioClient() {
         return MinioClient.builder()
-                .endpoint("http://127.0.0.1:9000")
+                .endpoint(minioUrl)
                 .credentials("minioadmin", "minioadmin")
                 .build();
     }
