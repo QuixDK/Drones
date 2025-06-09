136bc8dcb98828a5826429a039f3aa0766c2618a
dstroganov <dstroganov@dynamika.ru>
2025-06-10 00:25:03 +0700

    fix minio config

--- drones/src/main/java/ru/ssugt/drones/config/MinioConfig.java
+++ drones/src/main/java/ru/ssugt/drones/config/MinioConfig.java
@@ -1,5 +1,7 @@
 package ru.ssugt.drones.config;
 
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -9,12 +11,30 @@ import org.springframework.context.annotation.Configuration;
 public class MinioConfig {
 
     @Value("${spring.minio.endpoint}")
-    String minioUrl = "";
+    private String minioUrl;
+
+    @Value("${spring.minio.bucket:drones}")
+    private String bucketName;
     @Bean
-    public MinioClient minioClient() {
-        return MinioClient.builder()
+    public MinioClient minioClient() throws Exception {
+        MinioClient client = MinioClient.builder()
                 .endpoint(minioUrl)
                 .credentials("minioadmin", "minioadmin")
                 .build();
+
+        createBucketIfNotExists(client);
+        return client;
+    }
+
+    private void createBucketIfNotExists(MinioClient client) throws Exception {
+        boolean isExist = client.bucketExists(BucketExistsArgs.builder()
+                .bucket(bucketName)
+                .build());
+
+        if (!isExist) {
+            client.makeBucket(MakeBucketArgs.builder()
+                    .bucket(bucketName)
+                    .build());
+        }
     }
 }
